5d9b8d2ecc911cdd21e154d8aaefd9ed2aaada8d
Null <Null@Null.com>
2017-10-03 16:03:12 +0800

    Initial commit

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://store/admin/');
+define('HTTP_CATALOG', 'http://store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://store/admin/');
+define('HTTPS_CATALOG', 'http://store/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Null/Developer/store/admin/');
+define('DIR_SYSTEM', '/Users/Null/Developer/store/system/');
+define('DIR_IMAGE', '/Users/Null/Developer/store/image/');
+define('DIR_STORAGE', '/Users/Null/Developer/storage/');
+define('DIR_CATALOG', '/Users/Null/Developer/store/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '0086');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
