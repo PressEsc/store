cc35feeb9c2d6166139388c0a0e2fa33529db80b
Null <pressenter@163.com>
2017-10-15 16:17:22 +0200

    edit spanish contact page

--- catalog/language/es-es/information/contact.php
+++ catalog/language/es-es/information/contact.php
@@ -6,24 +6,24 @@ $_['heading_title']  = 'Contacto';
 $_['text_location']  = 'Donde Estamos';
 $_['text_store']     = 'Nuetras Tiendas';
 $_['text_contact']   = 'Formulario de Contacto';
-$_['text_address']   = 'Direcci&oacute;n';
-$_['text_telephone'] = 'Tel&eacute;fono';
+$_['text_address']   = 'Dirección';
+$_['text_telephone'] = 'Teléfono';
 $_['text_fax']       = 'Fax';
 $_['text_open']      = 'Horario';
 $_['text_comment']   = 'Comentario';
-$_['text_success']   = '<p>Su consulta ha sido enviada con exito a la dueño de la tienda!</p>';
+$_['text_success']   = '<p>Su consulta ha sido enviada con éxito a la dueño de la tienda!</p>';
 
 // Entry
 $_['entry_name']     = 'Su Nombre';
-$_['entry_email']    = 'Correo Electr&oacute;nico';
+$_['entry_email']    = 'Correo Electrónico';
 $_['entry_enquiry']  = 'Comentario';
-$_['entry_captcha']  = 'Introduzca el C&oacute;digo de la caja de abajo';
+$_['entry_captcha']  = 'Introduzca el código de la caja de abajo';
 
 // Email
 $_['email_subject']  = 'Comentario';
 
 // Errors
 $_['error_name']     = 'El nombre debe tener entre 3 y 32 caracteres!';
-$_['error_email']    = 'E-Mail no parece ser v&aacute;lido!';
+$_['error_email']    = 'E-Mail no parece ser válido!';
 $_['error_enquiry']  = 'Solicitud debe estar entre 10 y 3000 caracteres!';
-$_['error_captcha']  = 'C&oacute;digo de verificaci&oacute;n no coincide con la imagen!';
+$_['error_captcha']  = 'Código de verificación no coincide con la imagen!';
